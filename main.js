8fe6dec638cc4827d4b210a9aa2b4df509b1a948
Munaimun <munaimunz@gmail.com>
2023-11-18 16:49:40 +0600

    about page updated

--- main.js
+++ main.js
@@ -24,3 +24,13 @@ const linkAction = () => {
   navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// shadow header
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the shadow-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
