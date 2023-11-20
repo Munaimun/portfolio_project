04dd6060ac81f2d894675bace1084c3c4c9160e0
Munaimun <munaimunz@gmail.com>
2023-11-21 01:39:20 +0600

    project complete

--- main.js
+++ main.js
@@ -34,3 +34,68 @@ const shadowHeader = () => {
     : header.classList.remove("shadow-header");
 };
 window.addEventListener("scroll", shadowHeader);
+
+// ======email js========
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  //serviceID, templateId, form, publickey
+  emailjs
+    .sendForm(
+      "service_dxzuwyd",
+      "template_525ga59",
+      "#contact-form",
+      "Cco8iOajCvlWPD-9d"
+    )
+    .then(
+      () => {
+        contactMessage.textContent = "sent message succesfully";
+        //removing message
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 3000);
+        //clear input fields
+        contactForm.requestFullscreen();
+      },
+      () => {
+        contactMessage.textContent = "Message not sent!";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
+// ==========show scrollup===========
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
